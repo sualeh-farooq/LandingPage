197a3d868b9b070363b0327437ddac96df55f12b
tayaba <sufiyanfarooq047@gmail.com>
2025-01-27 03:44:59 +0500

    not done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"], // Match JavaScript/TypeScript files
+    languageOptions: {
+      ecmaVersion: 2021, // Modern JavaScript features
+      sourceType: "module", // Enable ES modules
+    },
+    rules: {
+      // Add custom rules here
+    },
+  },
 ];
+
+export default eslintConfig;
