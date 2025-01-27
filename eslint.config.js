e6885ab9447b76a49d0f8a4cf9400bb86c36c270
tayaba <sufiyanfarooq047@gmail.com>
2025-01-27 14:58:58 +0500

    not done

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals"),
+//   {
+//     files: ["**/*.{js,jsx,ts,tsx}"],
+//     languageOptions: {
+//       ecmaVersion: 2022,
+//       sourceType: "module",
+//       globals: {
+//         window: "readonly", // Browser global variables
+//         document: "readonly",
+//         navigator: "readonly",
+//         console: "readonly",
+//         process: "readonly", // Node.js global variables
+//         module: "readonly",
+//         require: "readonly",
+//       },
+//     },
+//     rules: {
+//       // Add any custom ESLint rules here
+//     },
+//   },
+// ];
+
+// export default eslintConfig;
+ 
