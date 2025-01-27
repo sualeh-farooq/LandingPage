bf14636e464d13c8e233a4fcdb769fd9b392db07
tayaba <sufiyanfarooq047@gmail.com>
2025-01-27 15:15:28 +0500

    not done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ['next'], // Extending Next.js's default ESLint config
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      'react/no-unescaped-entities': 'off', // Disabling unescaped entities rule
+      '@next/next/no-page-custom-font': 'off', // Disabling custom font warning rule
     },
   }),
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
